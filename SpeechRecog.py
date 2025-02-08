5021d46c1dff09234d900e810f7dba0f031710eb
jk03-legend <salcedo.320332@calamba.sti.edu.ph>
2025-02-08 14:27:27 +0800

    Update SpeechRecog.py

--- SpeechRecog.py
+++ SpeechRecog.py
@@ -19,4 +19,4 @@ def recognize_speech():
         return jsonify({"error": "Could not understand audio"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 1000)))
