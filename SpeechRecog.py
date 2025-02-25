d97c227e2f84637111ceed1b6f750c7a00c3916f
jk03-legend <salcedo.320332@calamba.sti.edu.ph>
2025-02-26 01:16:13 +0800

    Update SpeechRecog.py

--- SpeechRecog.py
+++ SpeechRecog.py
@@ -44,5 +44,5 @@ def recognize_speech():
     return jsonify({"text": result.strip() if result else "No speech detected"})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
